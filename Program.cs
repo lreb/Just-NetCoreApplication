d65f51745d60d15914b1853115f32a05a70027e2
Luis Espinoza <luis.espinoza@buildbinder.com>
2018-05-18 15:52:48 -0600

    enabled seeding

--- Program.cs
+++ Program.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using justtest.Models;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace justtest
@@ -14,7 +16,23 @@ namespace justtest
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            //BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    SeedData.Initialize(services);
+                }
+                catch (System.Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex,"An error occurred seeding the database.");
+                    throw;
+                }
+                host.Run();
+            }
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
